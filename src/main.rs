fdf6ca8087d02953df7316f5843fd04f4a68d552
NakagamiYuta <guguru0014@gmail.com>
2025-05-25 17:45:40 +0900

    [add] コマンドを仮で用意

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use bpaf::Bpaf;
+
+#[derive(Debug, Clone, Bpaf)]
+#[bpaf(options, version)]
+pub enum CliOptions {
+    /// Initialize the environment
+    #[bpaf(command)]
+    Init,
+
+    /// Update the all toolchains
+    #[bpaf(command)]
+    Update,
+
+    /// Set the default channel
+    #[bpaf(command)]
+    Default {
+        #[bpaf(positional)]
+        channel: String,
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    let opts = cli_options().run();
+    match opts {
+        CliOptions::Init => todo!(),
+        CliOptions::Update => todo!(),
+        CliOptions::Default { .. } => todo!(),
+    }
 }
