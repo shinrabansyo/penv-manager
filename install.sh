9b154ba58e7b0326b8c90856a781abaf921657ed
NakagamiYuta <guguru0014@gmail.com>
2025-05-25 20:40:53 +0900

    [fix] インストールスクリプトの実行がスタックしないように修正

--- install.sh
+++ install.sh
@@ -3,8 +3,12 @@ mkdir -p ~/.shinrabansyo/repos
 cd ~/.shinrabansyo/repos
 
 # 2. リポジトリのクローン
-git -c credential.helper='!f() { cat > /dev/null; echo username=; echo password=; }; f' clone https://github.com/shinrabansyo/penv-manager
-cd penv-manager
+git -c credential.helper='!f() { cat > /dev/null; echo username=; echo password=; }; f' clone https://github.com/shinrabansyo/penv-manager installer
+cd installer
 
 # 3. penv-manager を実行
 cargo run -- init
+
+# 4. インストーラを削除
+cd ../
+rm -rf installer
